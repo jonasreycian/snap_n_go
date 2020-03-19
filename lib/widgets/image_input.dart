43bc13b7ce67ac4472a7331047f34ee634950250
jonasreycian <cj.saraosos@gmail.com>
2020-03-20 02:47:50 +0800

    Done adding place screen and accessing camera

--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ImageInput extends StatefulWidget {
   @override
@@ -9,6 +10,14 @@ class ImageInput extends StatefulWidget {
 
 class _ImageInputState extends State<ImageInput> {
   File _storedImage;
+
+  Future<void> _takePicture() async {
+    final imageFile = await ImagePicker.pickImage(
+      source: ImageSource.camera,
+      maxWidth: 600,  // take size you really need
+      // maxHeight: 600
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -36,10 +45,10 @@ class _ImageInputState extends State<ImageInput> {
         ),
         Expanded(
           child: FlatButton.icon(
-            onPressed: () {},
             icon: Icon(Icons.camera),
             label: Text('Take Picture'),
             textColor: Theme.of(context).primaryColor,
+            onPressed: _takePicture,
           ),
         )
       ],
